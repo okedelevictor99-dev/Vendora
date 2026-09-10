c0e6ab3a2b78ab987bc3473c407ede54beae170a
Okedele Victor <your-email@example.com>
2026-09-10 14:43:44 +0100

    Configure server for deployment

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    const server = app.listen(PORT, () => {
+    const server = app.listen(PORT, "0.0.0.0", () => {
       logger.info(`🚀 Server running on port ${PORT}`);
     });
 
