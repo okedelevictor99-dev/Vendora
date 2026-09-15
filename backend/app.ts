2ea8072d96f20dea3ed611f3ff72b7765c70c6e2
Okedele Victor <your-email@example.com>
2026-09-15 13:38:31 +0100

    adding request logger to webhook route

--- backend/app.ts
+++ backend/app.ts
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
-app.use("/api/webhook", webhookRoutes);
+app.use("/api/webhook", requestLogger, webhookRoutes);
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
